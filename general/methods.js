a2757c5b053e06867ae261f9de671b364f1d04ea
zxcr9999 <91445089+hoaan1995@users.noreply.github.com>
2022-08-26 18:11:10 +0700

    update methods command

---
+++ general/methods.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+
+const ayarlar = require('../ayarlar.json');
+var room = ayarlar.commandroom;
+
+if (message.channel.id != room) {
+	return;
+  }
+
+// Example command
+if(!args[0]) {
+	const embed1 = new Discord.MessageEmbed()
+	.setColor('RANDOM')
+	.setTitle('🚀 **ZER0 BOT** 🚀')
+	.setDescription("**Layer 7** \n `HTTP-RAW` \n **Layer 4** \n `HOME`")
+	message.channel.send(embed1);
+	return;
+	}
+
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['methods'],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'methods',
+  description: 'zxcr9999',
+  usage: 'methods'
+}
